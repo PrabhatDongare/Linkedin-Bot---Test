155ab393c7abbd2d4f02e5aab848426f94086240
Prabhat-Dongare <prabhat@ungraytech.com>
2025-05-12 01:37:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import scrapeRoute from "./scrapeRoute.js";
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    try {
+        res.status(200).json({ success: true, message: "Backend is LIVE" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+});
+
+app.use(scrapeRoute);
+
+app.listen(port, () => {
+    console.log(`BACKEND listening on port ${port}`);
+});
